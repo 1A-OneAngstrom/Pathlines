780aa63a8332fa067c8d9653296ba2bc109bc0ad
Dmitriy Marin <dmitriy.marin@oneangstrom.com>
2023-02-28 11:22:03 +0100

    Add info in descriptor

--- Pathlines/include/SEPathlineOfCenterOfMassVisualModelDescriptor.hpp
+++ Pathlines/include/SEPathlineOfCenterOfMassVisualModelDescriptor.hpp
@@ -12,7 +12,12 @@
 SB_CLASS_BEGIN(SEPathlineOfCenterOfMassVisualModel);
 
 	SB_CLASS_TYPE(SBCClass::VisualModel);
+	SB_CLASS_PUBLIC_NAME("Pathline of the center of mass");
 	SB_CLASS_DESCRIPTION("Pathline of the center of mass");
+	SB_CLASS_TOOL_TIP("<b>Add a Pathline visual model</b><br/><br/>"
+		"The Pathline model allows you to visualize the movement of the center of mass of a system along a Path (trajectory).<br><br>"
+		"The Pathline model is created based on the current selection, or from the whole document if nothing is selected.<br/><br/>"
+		"A visual model is saved as a node in the document.");
 	SB_CLASS_VERSION_NUMBER("1.0.0");
 
 	SB_FACTORY_BEGIN;
